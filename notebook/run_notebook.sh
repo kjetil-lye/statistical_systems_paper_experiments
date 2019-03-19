c491a5bf0d1d59939495c84b45019e42ef193d1c
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-03-19 14:53:45 +0100

    added notebook to run experiments

---
+++ notebook/run_notebook.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+$HOME/.local/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute WassersteinDistancesPerturbationsAll.ipynb --output WassersteinDistancesPerturbationsAllOutput.ipynb
+
