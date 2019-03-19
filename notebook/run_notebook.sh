4df281f6370572fa8eacbd2b5ee4befeecdb1b4e
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-03-19 21:34:12 +0100

    added images

--- notebook/run_notebook.sh
+++ notebook/run_notebook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 $HOME/.local/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute WassersteinDistancesPerturbationsAll.ipynb --output WassersteinDistancesPerturbationsAllOutput.ipynb
+$HOME/.local/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute WassersteinDistances.ipynb --output WassersteinDistancesOutput.ipynb
 
